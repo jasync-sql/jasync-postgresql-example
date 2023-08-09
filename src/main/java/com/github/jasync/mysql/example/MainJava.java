74cfa478823fb82dec9fd5812ee7e572f567db81
oshai <ohadshai@gmail.com>
2023-08-09 21:25:10 +0300

    update examples

--- src/main/java/com/github/jasync/mysql/example/MainJava.java
+++ src/main/java/com/github/jasync/mysql/example/MainJava.java
@@ -1,19 +1,17 @@
 package com.github.jasync.mysql.example;
 
-import com.github.jasync.sql.db.Configuration;
+import com.github.jasync.sql.db.Connection;
 import com.github.jasync.sql.db.QueryResult;
 import com.github.jasync.sql.db.general.ArrayRowData;
 import com.github.jasync.sql.db.pool.ConnectionPool;
-import com.github.jasync.sql.db.pool.PoolConfiguration;
 import com.github.jasync.sql.db.postgresql.PostgreSQLConnection;
-import com.github.jasync.sql.db.postgresql.pool.PostgreSQLConnectionFactory;
+import com.github.jasync.sql.db.postgresql.PostgreSQLConnectionBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
 public class MainJava {
 
@@ -26,23 +24,15 @@ public class MainJava {
     logger.info("starting info");
     logger.debug("starting debug");
     logger.trace("starting trace");
-    Configuration configuration =
-      new Configuration(
-        "username",
-        "host.com",
-        5324,
-        "password",
-        "schema"
+    String host = "host.com";
+    int port = 5324;
+    String database = "schema";
+    String username = "username";
+    String password = "password";
+    ConnectionPool<PostgreSQLConnection> pool = PostgreSQLConnectionBuilder.createConnectionPool(
+            "jdbc:postgresql://$host:$port/$database?user=$username&password=$password"
     );
-    PoolConfiguration poolConfiguration = new PoolConfiguration(
-      100,                            // maxObjects
-      TimeUnit.MINUTES.toMillis(15),  // maxIdle
-      10_000,                         // maxQueueSize
-      TimeUnit.SECONDS.toMillis(30)   // validationInterval
-    );
-    ConnectionPool<PostgreSQLConnection> connection = new ConnectionPool<>(
-      new PostgreSQLConnectionFactory(configuration), poolConfiguration);
-    connection.connect().get();
+    Connection connection = pool.connect().get();
     CompletableFuture<QueryResult> future = connection.sendPreparedStatement("select * from table limit 2");
     QueryResult queryResult = future.get();
     System.out.println(Arrays.toString(((ArrayRowData) (queryResult.getRows().get(0))).getColumns()));
